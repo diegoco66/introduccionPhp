bffdf315a51d766bda8c43e66c9e3c37d2acd7c8
Diego Correa <diegoco66@gmail.com>
2019-11-11 18:35:25 -0500

    Haciendo un router con Aura.Router

--- public/index.php
+++ public/index.php
@@ -7,6 +7,7 @@ error_reporting(E_ALL);
 require_once  '../vendor/autoload.php';
 
 use Illuminate\Database\Capsule\Manager as Capsule;
+use Aura\Router\RouterContainer;
 
 $capsule = new Capsule;
 
@@ -34,4 +35,17 @@ $request = Zend\Diactoros\ServerRequestFactory::fromGlobals(
     $_FILES
 );
 
-var_dump($request->getUri()->getPath());
+$routerContainer = new RouterContainer();
+$map = $routerContainer->getMap();
+$map->get('index', '/introduccionPhp/', '../index.php');
+$map->get('addJob', '/introduccionPhp/jobs/add', '../addJob.php');
+$map->get('addProject', '/introduccionPhp/projects/add', '../addProject.php');
+
+$matcher = $routerContainer->getMatcher();
+$route = $matcher->match($request);
+
+if (!$route) {
+    echo 'No route found';
+} else {
+    require $route->handler;
+}
